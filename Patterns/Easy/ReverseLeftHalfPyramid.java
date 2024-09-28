6028b19f17341eacb1586d4d7a5de9117d3ceecc
Perugu Adithya <adithyaperugu2021@gmail.com>
2024-09-28 12:16:24 +0530

    pattern

---
+++ Patterns/Easy/ReverseLeftHalfPyramid.java
@@ -0,0 +1,85 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class ReverseLeftHalfPyramid {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number : ");
+		int number = sc.nextInt();
+		useNumber(number);
+		useStar(number);
+		useReverse(number);
+	}
+	public static void useNumber(int number) {
+		for(int i=number;i>=1;i--) {
+			//For loop for Spaces
+			for(int j=1;j<=number-i;j++) {
+				System.out.print(" ");
+			}
+			//For loop For Pattern
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+			
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void useStar(int number) {
+		for(int i=number;i>=1;i--) {
+			//For loop for Spaces
+			for(int j=1;j<=number-i;j++) {
+				System.out.print(" ");
+			}
+			//For loop For Pattern
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void useReverse(int number) {
+		for(int i=number;i>=1;i--) {
+			//For loop for Spaces
+			for(int j=1;j<=number-i;j++) {
+				System.out.print(" ");
+			}
+			//For loop For Pattern
+			for(int j=number;j>number-i;j--) {
+				System.out.print(j);
+			}
+			
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+
+/*
+
+12345
+ 1234
+  123
+   12
+    1
+
+*****
+ ****
+  ***
+   **
+    *
+ 
+ 54321
+  5432
+   543
+    54
+     5
+
+
+*/
